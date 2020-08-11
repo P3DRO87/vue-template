4ff2b574a05c6cdc6bb0c17b6453ef4a2d854565
Pedro <gokujose111@gmail.com>
2020-08-10 20:08:22 -0600

    fixing universal template

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
-  <v-container>
-    <h1 class="test">i am home components</h1>
-  </v-container>
+  <div class="wrap">
+    <v-container>
+      <h1 class="test">hola mundo with vuetify</h1>
+    </v-container>
+    <div class="container test">
+      <div class="row">
+        <div class="col-sm-auto mx-auto">
+          <h1>hola mundo with bootstrap</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
